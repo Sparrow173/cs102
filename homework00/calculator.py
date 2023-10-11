ab5202df59bb01c7100dab44063aaa2e4fbc62cf
Sparrow137 <veronvor.2005-5002@yandex.ru>
2023-10-11 14:05:45 +0300

    Добавлено решение

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -18,8 +18,9 @@ def calc(num_1: float, num_2: float, command: str) -> tp.Union[float, str]:
         tp.Union[float, str]: результат или ошибку
     """
     result = None
-    if command == "/" and num_2 == 0:
+    if command == "/" and num_2 == 0:  # проверка деления на ноль
         return "На ноль делить нельзя"
+    # проверка на логарифм от отрицательного числа
     if command in ("ln", "ln") and num_1 <= 0:
         return "Нельзя посчитать логарифм"
     if command in duo:
@@ -51,7 +52,7 @@ def calc(num_1: float, num_2: float, command: str) -> tp.Union[float, str]:
     return result
 
 
-def get_param(prompt: str) -> float:
+def get_param(prompt: str) -> float:  # функция получения числа
     """_summary_
 
     Args:
@@ -66,7 +67,7 @@ def get_param(prompt: str) -> float:
             result = float(input(prompt))
         except ValueError:  # Если возникла ошибка
             print("Ошибка ввода: Введите число")
-    return result
+    return result  # возвращаем результат
 
 
 uno = ("sin", "tan", "cos", "ln", "lg", "**2")
@@ -79,31 +80,34 @@ if __name__ == "__main__":
     while True:  # программа выполняется до ввода 0 вместо команды
         COMMAND = ""
         RESULT = None
-        NUM_1 = NUM_2 = NUM = 0.0
+        NUM_1 = NUM_2 = NUM = 0.0  # инициализируем переменные
         # отсеиваем неправильные операции
         while (COMMAND := input("Введите операцию > ").strip().lower()) not in commands:  # noqa
             print("Ошибка, введите другую операцию")
-        if COMMAND in duo:
-            NUM_1 = get_param("Первое число > ")
-            DONE = False
+        if COMMAND in duo:  # если команда с двумя операндами
+            NUM_1 = get_param("Первое число > ")  # получаем первое число
+            DONE = False  # устанавливаем флаг проверки допустимости операции
             while not DONE:
-                NUM_2 = get_param("Второе число > ")
+                NUM_2 = get_param("Второе число > ")  # получаем второе число
+                # если равно нулю, выводим сообщение об ошибке
                 if COMMAND in ["/"] and NUM_2 == 0:
                     print("На ноль делить нельзя")
                 else:
-                    DONE = True
-            # num_1 = float(input("Первое число > "))
-            # num_2 = float(input("Второе число > "))
+                    DONE = True  # все нормально
+
+            # получаем резальтат выислений
             RESULT = calc(NUM_1, NUM_2, COMMAND)
-        elif COMMAND in uno:
-            DONE = False
+        elif COMMAND in uno:  # если команда с одним операндом
+            DONE = False  # устанавливаем флаг проверки допустимости операции
             while not DONE:
-                NUM = get_param("Введите число > ")
+                NUM = get_param("Введите число > ")  # получаем число
+                # если значение равно нулю и логарифм
                 if COMMAND in ["lg", "ln"] and NUM <= 0:
+                    # сообщение об ршибке
                     print("Число может быть только положительным")
                 else:
-                    DONE = True
-            RESULT = calc(NUM, 0, COMMAND)
-        elif COMMAND == "0":
-            break
-        print(RESULT)
+                    DONE = True  # все нормально
+            RESULT = calc(NUM, 0, COMMAND)   # получаем резальтат выислений
+        elif COMMAND == "0":  # если команда '0'
+            break  # выходим
+        print(RESULT)  # вывод результата вычислений
