845ee3b778db2646f806a5e8c7d6866dbbaa51f3
Sparrow137 <veronvor.2005-5002@yandex.ru>
2023-12-19 21:08:06 +0300

    try

--- homework03/maze.py
+++ homework03/maze.py
@@ -140,7 +140,7 @@ def shortest_path(
     path.append(exit_coord)
 
     while grid[i][j] != 1:
-        if i > 0 and grid[i - 1][j] != '■' and grid[i - 1][j] == k - 1:
+        if i > 0 and grid[i - 1][j] != "■" and grid[i - 1][j] == k - 1:
             path.append((i - 1, j))
             i -= 1
             k -= 1
