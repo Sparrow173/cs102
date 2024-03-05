90cbe1e061c9043db6fce8866721f5d58d5241d0
Sparrow137 <veronvor.2005-5002@yandex.ru>
2024-03-06 01:45:09 +0300

    try

--- homework04/life_console.py
+++ homework04/life_console.py
@@ -10,28 +10,31 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """Отобразить рамку."""
-        screen.addstr(0, 0, "-" * (self.life.width + 2))
-        for row in range(self.life.height):
+        screen.addstr(0, 0, "-" * (self.life.cols + 2))
+        for row in range(self.life.rows):
             screen.addch(row + 1, 0, "|")
-            screen.addch(row + 1, self.life.width + 1, "|")
-        screen.addstr(self.life.height + 1, 0, "-" * (self.life.width + 2))
+            screen.addstr(row + 1, self.life.cols + 1, "|")
+        screen.addstr(self.life.rows + 1, 0, "-" * (self.life.cols + 2))
 
     def draw_grid(self, screen) -> None:
         """Отобразить состояние клеток."""
-        for row in range(self.life.height):
-            for col in range(self.life.width):
-                if self.life.grid[row][col] == 1:
+        for row in range(self.life.rows):
+            for col in range(self.life.cols):
+                if self.life.curr_generation[row][col] == 1:
                     screen.addch(row + 1, col + 1, "*")
 
     def run(self) -> None:
-        screen = curses.initscr()
-        curses.curs_set(0)
-
         try:
+            screen = curses.initscr()
+            curses.curs_set(0)
+            screen.clear()
+
             while True:
                 self.draw_borders(screen)
                 self.draw_grid(screen)
+                screen.refresh()
                 self.life.step()
+                curses.napms(500)
         except KeyboardInterrupt:
             pass
         finally:
@@ -39,11 +42,7 @@ class Console(UI):
 
 
 if __name__ == "__main__":
-    life = GameOfLife((5, 5))
+    life = GameOfLife((80, 40), max_generations=50)
     ui = Console(life)
     ui.run()
-    """
-    while life.is_changing and not life.is_max_generations_exceeded:
-        life.step()
-    """
     print(life.generations)
